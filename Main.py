5b1613a5d63e41edeb1b793d1f562b5ab5de38ea
vinegm <117782568+vinegm@users.noreply.github.com>
2023-04-26 14:54:11 -0300

    Update Main.py
    Testing QuickSort timing/comparisons/changes

--- Main.py
+++ Main.py
@@ -11,8 +11,23 @@ import QuickSort
 import RadixSort
 import ShellSort
 
+
 test_cases = TestCases.test_cases()
+
 print("-" * 75)
 for i, test in enumerate(test_cases):
-    print(f"Vetor{i+1}:\n{test}")
+    comparisons = 0
+    changes = 0
+
+    sortedArray, comparisons, changes = QuickSort.quick_sort_for_counting(test, 0, len(test)-1, comparisons, changes)
+
+    startTimer = timeit.default_timer()
+    sortedArray = QuickSort.quick_sort_for_timing(test, 0, len(test)-1)
+    stopTimer = timeit.default_timer()
+
+    print(f"Vetor{i+1}: \
+            \nForam feitas {comparisons} comparações e {changes} mudanças.\
+            \nFoi ordenado em {(stopTimer - startTimer):.6f} segundos.\
+            \nVetor ordenado:\n{sortedArray}")
+    
     print("-" * 75)
