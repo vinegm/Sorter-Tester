2689ee0a8a8a55649264887bf600545a1b9af584
vinegm <117782568+vinegm@users.noreply.github.com>
2023-04-27 18:11:22 -0300

    Better Testing+++
    Now every case is saved to a dict that in the future will be transformed into a data frame and shown as a graph (atm only shows one case at a time in the graph)

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import copy
 import timeit
 import os
 
@@ -18,16 +21,17 @@ def calculate_average_time(array_of_times):
     array_of_times (np.array): Array containing the samples
 
     Returns:
-    average_time: Time in average the algorithm took
+    average_time: Time in average the algorithm took (up to 6 decimals, a microsecond)
     """
     sum_of_times = 0
 
     for i in range(array_of_times.size):
         sum_of_times += array_of_times[i]
 
-    average_time = sum_of_times / array_of_times.size
+    average_time = "{:.6f}".format(sum_of_times / array_of_times.size)
 
     return average_time
+
 
 def test_timing(array, sorter, amount_of_tests):
     """Tests a sorter a given amout of times to get a average time
@@ -58,25 +62,61 @@ def test_timing(array, sorter, amount_of_tests):
 
     return average_time
 
+
 def test_sorter(test_case, sorter, amout_of_tests):
     average_time = test_timing(test_case, sorter[0], amout_of_tests)
     sortedArray, comparisons, swaps = sorter[1](test)
-    print(f"{sorter[0].__name__}:\
-            \nMade {comparisons} comparisons and {swaps} swaps;\
-            \nAverage time: {average_time:.6f}\
-            \nSorted array:\n{sortedArray}")
-    return 
+    # print(f"{sorter[0].__name__}:\
+    #         \nMade {comparisons} comparisons and {swaps} swaps;\
+    #         \nAverage time: {average_time:.6f}\
+    #         \nSorted array:\n{sortedArray}")
+    return average_time, comparisons, swaps
+
 
 sorters = np.array([[QuickSort.quick_sort, QuickSort.quick_sort_for_counting],
                     [SelectionSort.selectionSort, SelectionSort.selectionSort_counting]])
 
 test_cases = TestCases.test_cases()
-amount_of_tests = 250
+amount_of_tests = 1
+
+data = {"sorter": [],
+        "average time": [],
+        "comparisons": [],
+        "swaps": []}
+case = [copy.deepcopy(data) for i in range(len(test_cases))]
+
+
+for i, test in enumerate(test_cases):
+    for sorter in sorters:
+
+        average_time, comparisons, swaps = test_sorter(test, sorter, amount_of_tests)
+        
+        case[i]["sorter"].append(sorter[0].__name__)
+        case[i]["average time"].append(average_time)
+        case[i]["comparisons"].append(comparisons)
+        case[i]["swaps"].append(swaps)
+
 
 print("-" * 50)
-for i, test in enumerate(test_cases):
-    print(f"Vetor{i+1}: {test}")
+for i in range(len(case)):
+    print(f"Case{i+1}:")
+
+    for key, value in case[i].items():
+        print(key, value)
+
     print("-" * 50)
-    for sorter in sorters:
-        test_sorter(test, sorter, amount_of_tests)
-        print("-" * 50)
+
+
+
+df = pd.DataFrame(case[7])
+
+# Create a bar chart
+ax = df.plot(x='sorter', y=['comparisons', 'swaps'], kind='bar', figsize=(8, 6))
+
+# Add labels and title
+ax.set_xlabel("Sorters")
+ax.set_ylabel("Comparisons / Swaps")
+ax.set_title("Comparison of Sorters:")
+
+# Display the chart
+plt.show()
